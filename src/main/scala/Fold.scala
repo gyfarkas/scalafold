ad2c88a8b80209ff24b272af3c2390a724a80cb7
Gyorgy Farkas <gyfarkas@gmail.com>
2016-11-14 01:25:11 +0100

    formatting

--- src/main/scala/Fold.scala
+++ src/main/scala/Fold.scala
@@ -11,7 +11,7 @@ sealed trait Fold[A, B] {
   def summarize: C => B
   implicit val m: Monoid[C]
   def fold(xs: List[A]): B = summarize(xs.foldLeft(m.empty)((x, y) => x |+| tally(y)))
-  def run(xs: A*) : B = fold(xs.toList)
+  def run(xs: A*): B = fold(xs.toList)
 }
 
 object Fold {
@@ -22,9 +22,8 @@ object Fold {
     override def summarize: C => B = s
   }
 
-
-  implicit def applicativeFold[From] = new Applicative[({type F[X] = Fold[From,X]})#F] {
-    override def pure[A](x: A): Fold[From, A] = new Fold[From,A] {
+  implicit def applicativeFold[From] = new Applicative[({ type F[X] = Fold[From, X] })#F] {
+    override def pure[A](x: A): Fold[From, A] = new Fold[From, A] {
       override type C = HNil
 
       override def tally: (From) => HNil = _ => HNil
@@ -36,10 +35,10 @@ object Fold {
       private implicit val ffm: Monoid[ff.C] = ff.m
       private implicit val fam: Monoid[fa.C] = fa.m
       override type C = ff.C :: fa.C :: HNil
-      override implicit val m: Monoid[C] = MonoidInstances.hConsMonoid[ff.C , fa.C :: HNil]
+      override implicit val m: Monoid[C] = MonoidInstances.hConsMonoid[ff.C, fa.C :: HNil]
       override def tally: (From) => C = x => ff.tally(x) :: fa.tally(x) :: HNil
       override def summarize: C => B = x => {
-        val f  = ff.summarize(x.head)
+        val f = ff.summarize(x.head)
         val a = fa.summarize(x.tail.head)
         f(a)
       }
--- src/test/scala/MapReduceSpec.scala
+++ src/test/scala/MapReduceSpec.scala
@@ -1,13 +1,9 @@
 import org.scalatest._
 import cats._
-import cats.syntax.cartesian._
-import cats.syntax.monoid._
-import cats.syntax.applicative._
-import cats.syntax.functor._
+import cats.syntax.all._
 import cats.kernel.instances.all._
 import MonoidInstances._
 import Fold._
-import cats.syntax.CartesianBuilder
 
 case class SomeThing(i: Int, d: Double)
 
@@ -36,31 +32,46 @@ class MapReduceSpec extends WordSpec with Matchers {
     }
 
     "map over fold" in {
-      val f = Fold((x: SomeThing) => x, (x: SomeThing) => x) 
+      val f = Fold((x: SomeThing) => x, (x: SomeThing) => x)
       val g = f map (x => x.i)
       g.fold(List(SomeThing(1, 3.2), SomeThing(2, 4.5))) shouldEqual 3
     }
 
     "replacing monoid" in {
       val prodMonoid = new Monoid[Int] {
-        val empty =1
+        val empty = 1
         def combine(x: Int, y: Int) = x * y
       }
 
-      val length = Fold((s:String) => s.length, (l:Int) => s"the product of string length is ${l.toString}")(prodMonoid)
-      length.run("a","b") shouldEqual "the product of string length is 1"
+      val length = Fold((s: String) => s.length, (l: Int) => s"the product of string length is ${l.toString}")(prodMonoid)
+      length.run("a", "b") shouldEqual "the product of string length is 1"
     }
 
     "applicative average" in {
-      val sum = Fold((x: Int) => x,(x: Int) => x)
+      val sum = Fold((x: Int) => x, (x: Int) => x)
 
-      def length[A] = Fold((_:A) => 1, (x: Int) => x)
+      def length[A] = Fold((_: A) => 1, (x: Int) => x)
 
-      val average: Fold[Int,Double] =   sum |@| length map ((x,y) => x / y)
+      val average: Fold[Int, Double] = (sum |@| length) map ((x, y) => x / y)
 
       average.run(1,2,3) shouldEqual 2.0
     }
 
+    "word count" in {
+      val mapping = Fold(
+        (x: String) => List((x, 1)),
+        (x: List[(String, Int)]) => x
+      ).map(_.groupBy(_._1))
+
+      val reducing = Fold(
+        (m: Map[String, List[(String, Int)]]) => m.map { case (k, v) => (k, v.length) }.toList,
+        (x: List[(String, Int)]) => x
+      )
+
+      val result = reducing.run(mapping.run("abc", "abc", "def"))
+      result should contain theSameElementsAs List(("abc", 2), ("def", 1))
+    }
+
   }
-  
+
 }
